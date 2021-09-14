a9e33855637c7022661db96e5809d58c4501579b
Ethan Bird <eth51473@gmail.com>
2021-09-14 10:22:04 -0600

    more robust functionality

--- server.js
+++ server.js
@@ -11,19 +11,32 @@ let rollbar = new Rollbar({
   captureUncaught: true,
   captureUnhandledRejections: true
 })
+
+app.use("/style", express.static("./public/styles.css"))
+
 app.get("/",(req,res) => {
   res.sendFile(path.join(__dirname, "/public/index.html"))
   rollbar.info("HTML file served successfully!")
 })
 
-app.post("/api/student",(req,res) =>{
+app.post('/api/student', (req, res)=>{
   let {name} = req.body
-  name = name.trim();
-  students.push(name)
+  name = name.trim()
 
-  rollbar.log("Student added successfully", {author: "Ethan", type: "Manual entry"})
-  
-  res.status(200).send(students)
+  const index = students.findIndex(studentName=> studentName === name)
+
+  if(index === -1 && name !== ''){
+      students.push(name)
+      rollbar.log('Student added successfully', {author: 'Scott', type: 'manual entry'})
+      res.status(200).send(students)
+  } else if (name === ''){
+      rollbar.error('No name given')
+      res.status(400).send('must provide a name.')
+  } else {
+      rollbar.error('student already exists')
+      res.status(400).send('that student already exists')
+  }
+
 })
 
 
