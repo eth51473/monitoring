ce75b19f7d0a0eaf5baec055312abe6326f345e8
Ethan Bird <eth51473@gmail.com>
2021-09-14 09:22:06 -0600

    initialized rollbar

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const path = require("path");
-
+const Rollbar = require("rollbar")
 const app = express();
 
+let rollbar = new Rollbar({
+  accessToken: "a193da806b10417da3cf2ee4911e1364",
+  captureUncaught: true,
+  captureUnhandledRejections: true
+})
 app.get("/",(req,res) => {
   res.sendFile(path.join(__dirname, "/public/index.html"))
+  rollbar.info("HTML file served successfully!")
 })
+
+
 
 const port = process.env.PORT || 4545;
 
